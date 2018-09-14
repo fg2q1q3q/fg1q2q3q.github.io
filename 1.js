96949349cc96bf11eeb671709fe04af9636b041a
fg1q2q3q <43258920+fg1q2q3q@users.noreply.github.com>
2018-09-14 11:54:21 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,3 +1,3 @@
-document.writeln("<SPAN class=\"STYLE94\"><a href=\'/lz/?url=https://www.lanzous.com/i1txhwh&type=down'  target=\"_blank\">浪海飞魂多功能1.0</a></span>");
+document.writeln("<SPAN class=\"STYLE94\"><a href=\'/lz/?url=https://www.lanzous.com/i1txhwh&type=down'  target=\"_blank\">浪海飞魂1.0</a></span>");
 document.writeln("<SPAN class=\"STYLE94\"><a href=\'https://www.ikpay.cn/35dc50c0.html'  target=\"_blank\">自动24小时发卡购买</a></span>");
 document.writeln("<SPAN class=\"STYLE94\"><a href=\'http://help.juluoluo.com/656814'  target=\"_blank\">浪海飞魂功能介绍</a></span>");
