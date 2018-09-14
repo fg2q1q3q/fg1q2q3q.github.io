1d70e4bf21f00504b9bcc24f0e01de978bf8d9ff
fg1q2q3q <43258920+fg1q2q3q@users.noreply.github.com>
2018-09-14 11:33:29 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,3 @@
+document.writeln("<SPAN class=\"STYLE94\"><a href=\'/lz/?url=https://www.lanzous.com/i1txhwh&type=down'  target=\"_blank\">跑跑卡帕多功能2.94</a></span>");
+document.writeln("<SPAN class=\"STYLE94\"><a href=\'https://www.ikpay.cn/35dc50c0.html'  target=\"_blank\">自动24小时发卡购买</a></span>");
+document.writeln("<SPAN class=\"STYLE94\"><a href=\'http://help.juluoluo.com/656814'  target=\"_blank\">卡帕辅助功能介绍图片</a></span>");
