2e33168ff15509c034187c07684b534d1bcdb278
fg1q2q3q <43258920+fg1q2q3q@users.noreply.github.com>
2018-09-25 17:38:29 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,3 +1,3 @@
 document.writeln("<SPAN class=\"STYLE94\"><a   target=\"_blank\">浪海飞魂1.0</a></span>");
-document.writeln("<SPAN class=\"STYLE94\"><a   target=\"_blank\">请加群使用</a></span>");
 document.writeln("<SPAN class=\"STYLE94\"><a   target=\"_blank\">稳定免费</a></span>");
+document.writeln("<SPAN class=\"STYLE94\"><a   target=\"_blank\">请加群使用</a></span>");
