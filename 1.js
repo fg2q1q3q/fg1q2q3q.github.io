b5f6ef8fb62a99aa557f83f65378d2ffe4774877
fg1q2q3q <43258920+fg1q2q3q@users.noreply.github.com>
2018-09-25 17:35:14 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,3 +1,3 @@
 document.writeln("<SPAN class=\"STYLE94\"><a   target=\"_blank\">浪海飞魂1.0</a></span>");
-document.writeln("<SPAN class=\"STYLE94\"><a   target=\"_blank\">自动24小时发卡购买</a></span>");
-document.writeln("<SPAN class=\"STYLE94\"><a   target=\"_blank\">浪海飞魂功能介绍</a></span>");
+document.writeln("<SPAN class=\"STYLE94\"><a   target=\"_blank\">请加群使用</a></span>");
+document.writeln("<SPAN class=\"STYLE94\"><a   target=\"_blank\">稳定免费</a></span>");
